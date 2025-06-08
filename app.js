d3112f5b4f72cb29df8af7c723ec86e6b779e8bc
chaudharikunal12 <chaudharikunal314@gmail.com>
2025-06-08 10:19:32 +0530

    testing GET,POST

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express')
 
 const app = express()
 
-app.use('/',(req,res)=>{
-    res.send('send')
+app.get('/user', (req, res)=>{
+    res.send({name:"Kunal"})
+})
+app.post('/user', (req, res)=>{
+    res.send({name:"Kunal",
+        lastname:'Chaudhari'
+    })
+})
+app.use('/dashboard',(req,res)=>{
+    res.send('dashboard')
 })
 
-app.use('/test',(req,res)=>{
-    res.send('test')
+app.use('/',(req,res)=>{
+    res.send('Welcome')
 })
+
  app.listen(3000, ()=>{
     console.log('listened to 3000')
  })
